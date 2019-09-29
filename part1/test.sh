7c794a2cb0ad534fbad74e20188a3fafa8f1c9ba
wenyangteng <52967214+wenyangteng@users.noreply.github.com>
2019-09-29 15:13:10 -0400

    Update test.sh

--- part1/test.sh
+++ part1/test.sh
@@ -5,8 +5,8 @@
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /part1/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../access.log /part1/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
--file ../../part1/Mapper.py -mapper ../../part2/Mapper.py \
--file ../../part2/Reducer.py -reducer ../../part2/Reducer.py \
+-file ../../part1/Mapper.py -mapper ../../part1/Mapper.py \
+-file ../../part1/Reducer.py -reducer ../../part1/Reducer.py \
 -input /part1/input/* -output /part1/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /part1/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /part1/input/
