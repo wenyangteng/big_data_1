863eae1f10002f928e03a178f04b97e09fc04d61
wenyangteng <52967214+wenyangteng@users.noreply.github.com>
2019-09-29 15:02:30 -0400

    Update test.sh

--- part1/test.sh
+++ part1/test.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 ../../start.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/output/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /logstat2/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/access.log /logstat2/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /part1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /part1/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /part1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../access.log /part1/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
--file ../../mapreduce-test-python/logstat2/mapper.py -mapper ../../mapreduce-test-python/logstat2/mapper.py \
--file ../../mapreduce-test-python/logstat2/reducer.py -reducer ../../mapreduce-test-python/logstat2/reducer.py \
--input /logstat2/input/* -output /logstat2/output/
-/usr/local/hadoop/bin/hdfs dfs -cat /logstat2/output/part-00000
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/output/
+-file ../../part1/Mapper.py -mapper ../../part2/Mapper.py \
+-file ../../part2/Reducer.py -reducer ../../part2/Reducer.py \
+-input /part1/input/* -output /part1/output/
+/usr/local/hadoop/bin/hdfs dfs -cat /part1/output/part-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /part1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /part1/output/
 ../../stop.sh
