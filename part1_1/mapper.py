c072f0b0f6d0c549815c2b77855589520e735b9e
Ryan Tao <ryantao@Ryans-MacBook-Pro.local>
2019-10-23 18:59:45 -0400

    mapper_1

--- part1_1/mapper.py
+++ part1_1/mapper.py
@@ -4,5 +4,12 @@
 # When are tickets most likely to be issued?
 import re
 import sys
-pat = re.compile('(?P<time>\d\d\d\d')
-
+pat = re.compile('(?P<time>\d\d\d\d)(?P<tag>\D)')
+for line in sys.stdin:
+    match = pat.find(line)
+    if match:
+        hr = int(match.group(time)[:2])
+        mnt = int(match.group(time)[2:])
+        if match.group(tag) == 'P':
+            hr += 12
+        print('%s/t%s' % ('[' + str(hr) + ':' + str(mnt) + ']', 1))
