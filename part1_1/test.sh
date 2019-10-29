e7a88342f4df91c9c9fad5b5ee2c11248e7bb4af
RyanYuhan <44513080+RyanYuhan@users.noreply.github.com>
2019-10-29 19:35:47 -0400

    Update test.sh

--- part1_1/test.sh
+++ part1_1/test.sh
@@ -3,10 +3,10 @@ bash /mapreduce-test/start.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /prj_1/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /prj_1/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /prj_1/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal / /prj_1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /bigdataproject/big_data_1/inputdata.csv  /prj_1/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
--file -mapper  \
--file  -reducer  \
+-file /bigdataproject/big_data_1/part1_1/mapper.py -mapper /bigdataproject/big_data_1/part1_1/mapper.py \
+-file /bigdataproject/big_data_1/part1_1/reducer.py -reducer /bigdataproject/big_data_1/part1_1/reducer.py \
 -input /prj_1/input/* -output /prj_1/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /prj_1/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /prj_1/input/
