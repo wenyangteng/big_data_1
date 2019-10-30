e739b147a3fc1a8216794a6b1b896233d4e1c011
wenyangteng <52967214+wenyangteng@users.noreply.github.com>
2019-10-30 12:45:13 -0400

    Update test3.sh

--- part1_1/test3.sh
+++ part1_1/test3.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-bash /mapreduce-test/start.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /prj_1/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /prj_1/output/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /prj_1/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal / /prj_1/input/
+bash start.sh
+/usr/local/hadoop/bin/hdfs dfs -rm -r /part1_1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /part1_1/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /part1_1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../Parking_Violations_Issued_-_Fiscal_Year_2020.csv /part1_1/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
--file -mapper  \
--file  -reducer  \
--input /prj_1/input/* -output /prj_1/output/
-/usr/local/hadoop/bin/hdfs dfs -cat /prj_1/output/part-00000
-/usr/local/hadoop/bin/hdfs dfs -rm -r /prj_1/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /prj_1/output/
-bash /mapreduce-test/stop.sh
+-file ../../mapreduce-test-python/part1_1/mapper3.py -mapper ../part1_1/mapper3.py \
+-file ../../mapreduce-test-python/part1_1/reducer3.py -reducer ../part1_1/reducer3.py \
+-input /logstat/input/* -output /part1_1/output/
+/usr/local/hadoop/bin/hdfs dfs -cat /part1_1/output/part-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /part1_1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /part1_1/output/
+bash stop.sh
+
