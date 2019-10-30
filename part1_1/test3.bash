e0d5f96d8bf17dab5b51cda5972a287b025751cf
wenyangteng <52967214+wenyangteng@users.noreply.github.com>
2019-10-30 12:31:57 -0400

    Update test3.bash

--- part1_1/test3.bash
+++ part1_1/test3.bash
@@ -3,7 +3,7 @@ bash start.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /part1_1/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /part1_1/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /part1_1/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../Parking_Violations_Issued_-_Fiscal_Year_2020.csv /part1_1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../partial_data.csv /part1_1/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
 -file ../../mapreduce-test-python/part1_1/mapper3.py -mapper ../part1_1/mapper3.py \
 -file ../../mapreduce-test-python/part1_1/reducer3.py -reducer ../part1_1/reducer3.py \
